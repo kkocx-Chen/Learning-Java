6ec40372189e9db83f51ebfad563d75cac2ee432
陳政揚 <chenzhengyang@chenzhengyangs-MacBook-Pro.local>
2021-12-20 20:10:43 +0800

    成績計算 學習使用 Scanner 讓使用者輸入訊息

---
+++ A4/成績計算.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class 成績計算 {
+    public static void main(String[] args){
+        //輸入國文
+        Scanner CH = new Scanner(System.in);
+        System.out.print("1.請輸入國文");
+        int Chinese = CH.nextInt();
+        CH.close(); //關閉掃描儀 
+        //輸入英文
+        Scanner US = new Scanner(System.in);
+        System.out.print("2.請輸入英文");
+        int English = US.nextInt();
+        US.close(); //關閉掃描儀 
+        //輸入數學
+        Scanner MT = new Scanner(System.in);
+        System.out.print("3.請輸入數學");
+        int Math = MT.nextInt();
+        MT.close(); //關閉掃描儀 
+        //總分
+        int total = Chinese + English + Math;
+        System.out.println("總成績"+total);
+        //平均 
+        double ave = (double)total / 3 ; //強制轉型只限制數字 
+        System.out.println("平均"+ave);
+        //if else
+        if (ave >= 60){
+            System.out.println("恭喜及格");
+        }else{
+            System.out.println("抱歉補考");
+        }
+    }
+    
+}
