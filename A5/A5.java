a936268fa953bf3070b85e3170ed6de97611c4ad
陳政揚 <chenzhengyang@chenzhengyangs-MacBook-Pro.local>
2021-12-22 21:45:52 +0800

    有關型態

---
+++ A5/A5.java
@@ -0,0 +1,14 @@
+public class A5
+{
+    public static void main (String A[])
+    {
+        char C1 = 'a' , C2 = 'b';
+        System.out.println("c1="+C1+" c2="+C2);
+        int i1 = 10 , i2 = (int)2.5 , i3='A';
+        System.out.println("i1= "+i1+" i2="+i2+" i3="+i3);
+        float f1 = 5 , f2 = 'a' ,f3 = (float)2.5;
+        System.out.println("f1="+f1+" f2="+f2+" f3="+f3);
+        boolean b1 = true , b2 = false ;
+        System.out.println("b1="+b1+" b2="+b2);
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,19 @@ Learning Java
 掃描儀用於掃描用戶之輸入
 掃描完成後必須關閉掃描儀    
 否則報錯 **test.close**();  
+# 基本資料型態
+2 字元 char     
+1 整數 byte     
+2 整數 short    
+4 整數 int  
+8 整數 long     
+4 浮點數 float      
+8 浮點數 double     
+1 布林 boolean      
+參考資料型態 字串 String 陣列 Away 物件 object  
+#### 宣告變數   
+資料型態 變數名稱
+#### 存入面數 
+變數名稱 = 內容
+#### 取出變數
+Global 儲存 static 資訊
