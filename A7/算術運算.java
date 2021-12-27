93ecf0f6daad3833826a735e74a1f06f65948485
陳政揚 <chenzhengyang@chenzhengyangs-MacBook-Pro.local>
2021-12-27 21:31:29 +0800

    算術運算

---
+++ A7/算術運算.java
@@ -0,0 +1,16 @@
+public class 算術運算 {
+    public static void main(String A[])
+    {
+        int a = 155;
+        int b = 9;
+        float g;
+        g = a/b ;
+        System.out.println("g = a/b ; , g="+g);
+        g = (float)a/b;
+        System.out.println("(float)a/b , g="+g);
+        g = (int)(a/9.0);
+        System.out.println("(int)a/9.0 , g="+g);
+
+    }
+    
+}
